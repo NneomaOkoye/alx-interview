2074cba9923db90cb54f0644a8e7fc5b300c4ac0
NneomaOkoye <okoyenneoma1@gmail.com>
2023-11-12 12:57:39 -0800

    done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,36 @@
 #!/usr/bin/node
-// make request to star wars api
+const argv = process.argv;
+const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
+const urlMovie = `${urlFilm}${argv[2]}/`;
 
 const request = require('request');
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films';
 
-const movie = process.argv.slice(2)[0];
+request(urlMovie, function (error, response, body) {
+  if (error == null) {
+    const fbody = JSON.parse(body);
+    const characters = fbody.characters;
 
-// handle api requests using request module
-const apiRequest = (url) => {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(body);
-      }
-    });
-  });
-};
-
-// main function
-const makeRequest = async () => {
-  try {
-    const response = await apiRequest(`${apiUrl}/${movie}`);
-    const data = JSON.parse(response);
-    const characters = data.characters;
-    for (const characterUrl of characters) {
-      const res = await apiRequest(characterUrl);
-      const character = JSON.parse(res);
-      console.log(character.name);
+    if (characters && characters.length > 0) {
+      const limit = characters.length;
+      CharRequest(0, characters[0], characters, limit);
     }
-  } catch (err) {
-    console.log(err);
+  } else {
+    console.log(error);
   }
-};
+});
 
-makeRequest();
+function CharRequest (idx, url, characters, limit) {
+  if (idx === limit) {
+    return;
+  }
+  request(url, function (error, response, body) {
+    if (!error) {
+      const rbody = JSON.parse(body);
+      console.log(rbody.name);
+      idx++;
+      CharRequest(idx, characters[idx], characters, limit);
+    } else {
+      console.error('error:', error);
+    }
+  });
+}
