eb4623270608790a6edc82545a3ab986597ff2d5
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-11-23 13:28:27 +0100

    Update 0-making_change.py
    done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-"""
-This module contains the make change function
-"""
+'''
+Given a pile of coins of different values, determine the fewest number of
+coins needed to meet a given total total.
+'''
 
 
 def makeChange(coins, total):
-    """returns fewest number of coins to meet total"""
+    """
+    For a given few coins
+
+    Args:
+        coins: List of coin denominations available.
+        total: Integer value for the target total.
+
+    Returns:
+        Integer: The fewest number of coins needed to meet the total.
+        If the total is 0 or less, return 0.
+        If the total cannot be met by any number of coins available, return -1.
+    """
     if total <= 0:
         return 0
 
-    min_coins = [0] + [total + 1] * total
+    coins.sort(reverse=True)  # Sort the coin denominations in descending order
+    num_coins = 0
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i - coin >= 0:
-                min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            num_coins += 1
 
-    if min_coins[total] < total:
-        return min_coins[total]
-
-    return -1
+    return num_coins if total == 0 else -1
